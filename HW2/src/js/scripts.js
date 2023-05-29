75fda05896c7ed4822e5c1a0949997990073c7c3
Ali Ramazanov <aliirmznv@gmail.com>
2023-05-29 13:49:47 +0400

    Updated a few things

--- HW2/src/js/scripts.js
+++ HW2/src/js/scripts.js
@@ -4,17 +4,17 @@ const navbar = document.querySelector(".navbar");
 
 mobileOpenBtn.addEventListener("click", () => {
   navbar.classList.add("open");
-  mobileCloseBtn.style.display = "block"; // Show the mobile close button
+  mobileCloseBtn.style.display = "block"; // 
 });
 
 mobileCloseBtn.addEventListener("click", () => {
   navbar.classList.remove("open");
-  mobileCloseBtn.style.display = "none"; // Hide the mobile close button
+  mobileCloseBtn.style.display = "none"; 
 });
 
 window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
     navbar.classList.remove("open");
-    mobileCloseBtn.style.display = "none"; // Hide the mobile close button
+    mobileCloseBtn.style.display = "none"; 
   }
 });
