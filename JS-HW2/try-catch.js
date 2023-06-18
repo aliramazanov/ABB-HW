bf6c25fa37f483c687f4494c5b7af3e2d18010f1
Ali Ramazanov <aliirmznv@gmail.com>
2023-06-18 23:00:23 +0400

    Added theoretical part

---
+++ JS-HW2/try-catch.js
@@ -0,0 +1,14 @@
+/* 
+
+-----> Note: This method only works with code which runs (valid code) <----
+
+When an error happens in our JavaScript code, it usually stops running
+To handle this situation, and let the code to keep running despite the error
+we are using try...catch.
+
+try...catch contains two parts - the try part is where the first the the code 
+runs and the catch. If there is not an error, catch part doesn't run. 
+If there is an error the catch part runs and we get an error, 
+which we can also define its message.
+
+*/
