8be927e7c4fbcb1a848bd966999b23ce98d643f3
Ali Ramazanov <aliirmznv@gmail.com>
2023-06-22 00:01:11 +0400

    Completed

--- JS-HW3/destructuring.js
+++ JS-HW3/destructuring.js
@@ -111,6 +111,40 @@ console.log(`Is ${nameOfUser} admin: ${isAdmin}`);
 
 /* Task 4 */
 
+const satoshi2020 = {
+  name: "Nick",
+  surname: "Sabo",
+  age: 51,
+  country: "Japan",
+  birth: "1979-08-21",
+  location: {
+    lat: 38.869422,
+    lng: 139.876632,
+  },
+};
+
+const satoshi2019 = {
+  name: "Dorian",
+  surname: "Nakamoto",
+  age: 44,
+  hidden: true,
+  country: "USA",
+  wallet: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa",
+  browser: "Chrome",
+};
+
+const satoshi2018 = {
+  name: "Satoshi",
+  surname: "Nakamoto",
+  technology: "Bitcoin",
+  country: "Japan",
+  browser: "Tor",
+  birth: "1975-04-05",
+};
+
+const allCollectedData = { ...satoshi2018, ...satoshi2019, ...satoshi2020 };
+console.log(allCollectedData);
+
 /* 
 Task 5
 You are given an array of books. 
