4c7baadcd3b8b484e53355c09ff91a41411e846a
Ali Ramazanov <aliirmznv@gmail.com>
2023-06-26 20:32:30 +0400

    Updates

--- JS-HW4/ajax.js
+++ JS-HW4/ajax.js
@@ -1,13 +1,7 @@
 function collectData() {
   const xReq = new XMLHttpRequest();
   xReq.open("GET", "https://ajax.test-danit.com/api/swapi/films", true);
-
-  /* xReq.onload = function () {
-    if (this.status == 200) {
-      console.log(this.responseText);
-    }
-  }; */
-
+  
   xReq.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       // console.log(this.responseText);
