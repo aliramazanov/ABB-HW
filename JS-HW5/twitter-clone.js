934ba955c1003a7eabf2dbb03c57e13bcb57707e
Ali Ramazanov <aliirmznv@gmail.com>
2023-07-09 23:18:49 +0400

    Finalized Twitter Clone HW

--- JS-HW5/twitter-clone.js
+++ JS-HW5/twitter-clone.js
@@ -1,22 +1,57 @@
-const getDataButton = document.querySelector("#get-data");
-const loaderContainer = document.querySelector(".loader-container");
+const fetchUsers = fetch("https://ajax.test-danit.com/api/json/users").then(
+  (response) => response.json(),
+);
 
-getDataButton.addEventListener("click", () => {
-  loaderContainer.style.display = "flex";
+const fetchPosts = fetch("https://ajax.test-danit.com/api/json/posts").then(
+  (response) => response.json(),
+);
 
-  const fetchUsers = fetch("https://ajax.test-danit.com/api/json/users")
-    .then((response) => response.json())
-    .then((users) => {
-      console.log("Users:", users);
-    });
-
-  const fetchPosts = fetch("https://ajax.test-danit.com/api/json/posts")
-    .then((response) => response.json())
-    .then((posts) => {
-      console.log("Posts:", posts);
-    });
-
-  Promise.all([fetchUsers, fetchPosts]).then(() => {
-    loaderContainer.style.display = "none";
-  });
+Promise.all([fetchUsers, fetchPosts]).then(([users, posts]) => {
+  displayData(users, posts);
 });
+
+function displayData(users, posts) {
+  const postContainer = document.getElementById("post-container");
+
+  const postWrapper = document.createElement("div");
+
+  posts.forEach((post) => {
+    const user = users.find((user) => user.id === post.userId);
+    const profileImageURL = `https://i.pravatar.cc/150?u=${Math.random()}`;
+
+    const postCard = document.createElement("div");
+    postCard.className = "post-card";
+    postCard.setAttribute("data-id", post.id);
+
+    postCard.innerHTML = `
+      <img src="${profileImageURL}" alt="Profile Image" class="profile-image">
+      <div class="post-info">
+        <h2 class="user-name">${user.name}</h2>
+        <p class="post-content">${post.body}</p>
+      </div>
+      <button class="delete-button">Delete</button>
+    `;
+
+    postWrapper.appendChild(postCard);
+  });
+
+  postContainer.appendChild(postWrapper);
+  document.body.appendChild(postContainer);
+
+  postContainer.addEventListener("click", (event) => {
+    if (event.target.matches(".delete-button")) {
+      handleDelete(event);
+    }
+  });
+}
+
+function handleDelete(clickEvent) {
+  const postCard = clickEvent.target.closest(".post-card");
+  const postId = postCard.getAttribute("data-id");
+
+  fetch(`https://ajax.test-danit.com/api/json/posts/${postId}`, {
+    method: "DELETE",
+  }).then(() => {
+    postCard.remove();
+  });
+}
